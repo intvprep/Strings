7b58ef3e8eeec1d0e580d6f651b785b7faf79db8
rs873m <rs873m@att.com>
2017-04-20 20:35:47 -0500

    substring brute force.

--- src/SubString.java
+++ src/SubString.java
@@ -3,18 +3,27 @@
 public class SubString {
 
 	
-//	public static int subString(String txt, String pattern){
-//		
-//		if(pattern.length() > txt.length()) return -1;
-//		
-//		char[] searchIn = txt.toCharArray();
-//		char[] search = pattern.toCharArray();
-//		
-//		
-//		for (int i = 0; i < searchIn.length; i++) {
-//			int k=0;
-//			//while(k<)
-//		}
-//		
-//	}
+	public static void main(String[] args) {
+		System.out.println(subString("aaaaddddbbddd", "aad"));
+	}
+	
+	public static int subString(String txt, String pattern){
+		
+		if(pattern.length() > txt.length()) return -1;
+		
+		char[] searchIn = txt.toCharArray();
+		char[] search = pattern.toCharArray();
+		
+		for (int i = 0; i < searchIn.length; i++) {
+			int k=0;
+			boolean continu = true;
+			while(k<search.length && continu){
+				continu = search[k]==searchIn[i+k];
+				if(continu)k++;
+			}
+			if(k == search.length) return i;
+		}
+		return -1;
+		
+	}
 }	
