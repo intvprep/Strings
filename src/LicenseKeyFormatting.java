677bea447cc0c92d4caed8ea9c8593f8e8969e4a
intv prep <eraised2pii@gmail.com>
2017-10-24 21:40:21 -0400

    converting to git for easy import in eclipse

--- src/LicenseKeyFormatting.java
+++ src/LicenseKeyFormatting.java
@@ -13,9 +13,19 @@ public class LicenseKeyFormatting {
         return sb.reverse().toString();
     }
     
+    //from leetcode
+    public String licenseKeyFormatting2(String s, int k) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = s.length() - 1; i >= 0; i--)
+            if (s.charAt(i) != '-')
+                sb.append(sb.length() % (k + 1) == k ? '-' : "").append(s.charAt(i));
+        return sb.reverse().toString().toUpperCase();
+    } 
+     
     public static void main(String[] args) {
 		LicenseKeyFormatting s = new LicenseKeyFormatting();
 		System.out.println(s.licenseKeyFormatting("2-4A0r7-4k", 3));
+		System.out.println(s.licenseKeyFormatting2("2-4A0r7-4k", 3));
 	}
 }
 
